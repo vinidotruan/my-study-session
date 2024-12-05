85b23975bf031f4492c523b3abf93e934e35df9b
Vini <contatoruanvinicius@gmail.com>
2024-12-04 23:18:59 -0300

    testing

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -58,6 +58,7 @@ return [
                 'scheme' => env('PUSHER_SCHEME', 'https'),
                 'encrypted' => true,
                 'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
+                'path' => '/teste'
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
